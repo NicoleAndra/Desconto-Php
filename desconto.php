bee6f56776c0f079abc0a7c640b4a7246ae1f15d
Nicole Cândido <129750120+NicoleAndra@users.noreply.github.com>
2024-08-27 09:08:32 -0300

    Add files via upload

---
+++ desconto.php
@@ -0,0 +1,40 @@
+<!--HTML apenas para melhorar a saída do PHP-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <center>
+        <h1>
+<!--Código PHP-->
+<?php
+//Funcao da calculadora:
+function calcular($n1, $n2){
+    if ($n2 <= 1){
+        $nd = $n1 * 0.10;
+        $nt = $n1 - $nd;
+        echo ("Valor a pagar: R$$nt");
+    }
+    elseif ($n2 > 0 && $n2 < 4 ){
+        $nd = $n1 * 0.05;
+        $nt = $n1 - $nd;
+        echo ("Valor a pagar: R$$nt");
+    }
+    elseif ($n2 >= 4){
+        $nd = $n1 * 0.10;
+        $nt = $n1 + $nd;
+        echo ("Valor a pagar: R$$nt");
+    }
+    else{
+        echo "Tente novamente";
+    }
+}
+//Chamando a funcao
+calcular($n1 = $_POST ["n1"], $n2 = $_POST ["n2"])
+?>
+        <br>
+        <a href="site.php"> voltar </a> 
+        </h1>
+    </center>
+    </body>
+</html>
---
+++ recebe.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link  href="css/bootstrap.css" rel="stylesheet">
+    <title>Calculadora</title>
+</head>
+	<body>
+		<form method="POST" action="desconto.php">
+		 <center>
+        <fieldset>
+        <h3>Insira o valor da venda e a quantidade de parcelas:</h3>
+        Valor da venda: <input type="text" name="n1"><br><br>
+        Quantidade de parcelas: <input type="text" name="n2"><br><br>
+        <button type="submit" class="btn btn-primary" name="acao">Enviar</button>
+    </fieldset>
+		 </center>
+		</form>
+	</body>
+</html>
